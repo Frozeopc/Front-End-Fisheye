b9b663519ed935727183fdd2b1c9e40af524e09b
Vincent Monchatre <vincentmonchatre@gmail.com>
2022-06-03 19:24:41 +0200

    formulaire

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,3 +1,21 @@
+const inputFirstName = document.getElementById("prenom")
+const inputLastName = document.getElementById("Nom")
+const inputMail = document.getElementById("mail")
+const inputMessage = document.getElementById("message")
+const inputSubmit = document.getElementById("contact_button")
+
+
+inputSubmit.addEventListener("click", function (event) {
+    ;
+    console.log(inputFirstName.value);
+    console.log(inputLastName.value);
+    console.log(inputMail.value);
+    console.log(inputMessage.value);
+    closeModal();
+    event.preventDefault(); return false;
+}, false);
+
+
 function displayModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "block";
